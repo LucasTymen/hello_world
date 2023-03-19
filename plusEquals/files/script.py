1d43329efe512d96a746dcf7348e9af777396e82
Lucas Tymen <lucas.tymen@gmail.com>
2023-03-19 16:38:58 +0100

    plusEqual_done

--- plusEquals/files/script.py
+++ plusEquals/files/script.py
@@ -5,7 +5,12 @@ new_sneakers = 50.00
 total_price += new_sneakers
 
 nice_sweater = 39.00
+
+total_price += nice_sweater
+
 fun_books = 20.00
+
+total_price += fun_books
 # Update total_price here:
 
 print("The total price is", total_price)
