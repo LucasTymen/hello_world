752db6076c065bc94fe2ed7bce401658e164bef3
Lucas Tymen <lucas.tymen@gmail.com>
2023-03-19 17:28:20 +0100

    multiLineStrings

---
+++ review.py
@@ -0,0 +1,39 @@
+"""
+Hello World
+Review
+
+In this lesson, we accomplished a lot of things! We instructed our computers to print messages, we stored these messages
+as variables, and we learned to update those messages depending on the part of the program we were in. We performed
+mathematical calculations and explored some of the mathematical expressions that Python offers us. We learned about
+errors and other valuable skills that will continue to serve us as we develop our programming skills.
+
+Good job!
+
+Here are a few more resources to add to your toolkit:
+
+    Codecademy Docs: Python (https://www.codecademy.com/resources/docs/python?page_ref=catalog)
+    Codecademy Workspaces: Python (https://www.codecademy.com/workspaces/new)
+
+Make sure to bookmark these links so you have them at your disposal.
+
+last exercise :
+Create variables:
+
+    my_age
+    half_my_age
+    greeting
+    name
+    greeting_with_name
+
+Assign values to each using your knowledge of division and concatenation!
+
+my code :
+"""
+my_age = 51
+half_my_age = my_age/2
+greeting = "Hello World"
+name = "Lucas"
+greeting_with_name = print(greeting + name)
+"""
+
+"""
