8e2876515d62ca33db9ceb986511924de77acb8d
Lucas Tymen <lucas.tymen@gmail.com>
2023-03-20 14:28:55 +0100

    EqualsandNotEquals

---
+++ EqualsandNotEquals/theory_EqualsandNotEquals.py
@@ -0,0 +1,53 @@
+"""
+My Home
+Control Flow: Relational Operators: Equals and Not Equals
+Learn
+Control Flow
+Relational Operators: Equals and Not Equals
+
+Now that we understand what boolean expressions are, let’s learn to create them in Python. We can create a boolean expression by using relational operators.
+
+Relational operators compare two items and return either True or False. For this reason, you will sometimes hear them called comparators.
+
+The two relational operators we’ll cover first are:
+
+    Equals: ==
+    Not equals: !=
+
+These operators compare two items and return True or False if they are equal or not.
+
+We can create boolean expressions by comparing two values using these operators:
+"""
+1 == 1     # True
+
+2 != 4     # True
+
+3 == 5     # False
+
+'7' == 7   # False
+
+"""Each of these is an example of a boolean expression.
+
+Why is the last statement false? The '' marks in '7' make it a string, which is different from the integer value 7, so they are not equal. When using relational operators it is important to always be mindful of type.
+Instructions
+1.
+
+Determine if the following boolean expressions are True or False. Input your answer as True or False in the appropriate variable to the right.
+
+Statement one:
+"""
+(5 * 2) - 1 == 8 + 1
+
+"""Statement two:
+"""
+13 - 6 != (3 * 2) + 1
+
+"""Statement three:
+"""
+3 * (2 - 1) == 4 - 1
+
+"""Simplify each side of the expressions using basic order of operations, then determine if the expression is true or false. We want to use the boolean values, so make sure to use True and False rather than "True" and "False".
+Concept Review
+Want to quickly review some of the concepts you’ve been learning? Take a look at this material's cheatsheet!
+https://www.codecademy.com/learn/learn-python-3/modules/learn-python3-control-flow/cheatsheet
+"""
---
+++ booleanExpressions/theory_booleanExpressions.py
@@ -0,0 +1,57 @@
+"""Control Flow
+Boolean Expressions
+
+In order to build control flow into our program, we want to be able to check if something is true or not. A boolean expression is a statement that can either be True or False.
+
+Let’s go back to the ‘waking up’ example. The first question, “Is today a weekday?” can be written as a boolean expression:
+
+Today is a weekday.
+
+This expression can be True if today is Tuesday, or it can be False if today is Saturday. There are no other options.
+
+Consider the phrase:
+
+Friday is the best day of the week.
+
+Is this a boolean expression?
+
+No, this statement is an opinion and is not objectively True or False. Someone else might say that “Wednesday is the best weekday,” and their statement would be no less True or False than the one above since there is no objective answer to what the best day of the week is.
+
+How about the phrase:
+
+Sunday starts with the letter 'C'.
+
+Is this a boolean expression?
+
+Yes! This expression can only be True or False, which makes it a boolean expression. Even though the statement itself is false (Sunday starts with the letter ‘C’), it is still a boolean expression.
+Instructions
+1.
+
+Determine if the following statements are boolean expressions or not. If they are, set the matching variable to the right to "Yes" and if not set the variable to "No". Here’s an example of what to do:
+
+Example statement:
+
+My dog is the cutest dog in the world.
+
+This is an opinion and not a boolean expression, so you would set example_statement to "No" in the editor to the right. Okay, now it’s your turn:
+
+Statement one:
+
+Dogs are mammals.
+
+Statement two:
+
+My dog is named Pavel.
+
+Statement three:
+
+Dogs make the best pets.
+
+Statement four:
+
+Cats are female dogs.
+
+For each example, consider the following questions: Is this objectively true or false? If so, answer “Yes,” since it’s a boolean statement. If not, answer “No,” since it’s an opinion.
+Concept Review
+Want to quickly review some of the concepts you’ve been learning? Take a look at this material's cheatsheet!
+"""
