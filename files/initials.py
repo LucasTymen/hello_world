a44ccdba964567fc30755500d508b8c847c2c9e3
Lucas Tymen <lucas.tymen@gmail.com>
2023-03-20 10:43:31 +0100

    ascii

---
+++ files/initials.py
@@ -0,0 +1,29 @@
+# Lucas Tymen
+# Developper Ruby-on-Rails, Javascript and soon on Python
+ascii = """
+ __,    _, _,  ____,   ____, ____,    ____,  _  _,  __, _, ____,  _,  _,
+(-|    (-|  \ (-/     (-/_| (-(__    (-|    (-\_/  (-|\/| (-|_,  (-|\ |
+ _|__,  _|__/  _\__,  _/  |, ____)    _|,     _|,   _| _|, _|__,  _| \|,
+(      (      (      (      (        (       (     (      (      (
+"""
+first_name = """
+L
+L
+L
+L
+L
+L
+LLLLL
+"""
+
+last_name = """
+TTTTT
+  T
+  T
+  T
+  T
+  T
+  T
+"""
+
+print(ascii + first_name + last_name)
