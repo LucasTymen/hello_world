cd1f3b6242caedfd88ffb73f1963476b506de99f
Lucas Tymen <lucas.tymen@gmail.com>
2023-03-21 11:02:51 +0100

    BooleanOperators_or

--- BooleanOperators_or/BooleanOperators_or.py
+++ BooleanOperators_or/BooleanOperators_or.py
@@ -73,5 +73,6 @@ statement_two = (9 + 5 <= 15) or (7 != 4 + 3)
 credits = 118
 gpa = 2.0
 
-if credits >= 120 or gpa >= 2.0 : print("You have met at least one of the requirements.")
+if credits >= 120 or gpa >= 2.0 :
+  print("You have met at least one of the requirements.")
 """
